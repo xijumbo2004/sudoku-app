9e8517c659cc41cba99a5ffe40556ab85874f0ff
xijumbo2004 <37528032+xijumbo2004@users.noreply.github.com>
2026-05-22 13:32:17 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  // 何もしない（オンライン専用）
+});
